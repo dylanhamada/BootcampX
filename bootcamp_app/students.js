763cb23cf8dc39f8cfa11d8728f65bcf38a1f9f1
dylanhamada <dylan.hamada@gmail.com>
2023-11-16 13:16:31 -0500

    Modify Queries to Use Parameterized Queries
    - Modify students.js and teachers.js to use parameterized queries instead of string literals

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 
-const [execPath, file, cohort, maxResults] = process.argv;
+const [execPath, file, cohortName, maxResults] = process.argv;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -9,13 +9,16 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT students.id as id, students.name as name, cohorts.name as cohort
   FROM students
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${cohort}%'
-  LIMIT ${maxResults || 5};
-`)
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+`;
+const values = [`%${cohortName}%`, maxResults];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM assistance_requests
   JOIN teachers ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${cohortName}%'
+  WHERE cohorts.name LIKE $1
   ORDER BY teachers.name ASC;
-`)
+`;
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
 .then(res => {
   console.log(res.rows);
 })
